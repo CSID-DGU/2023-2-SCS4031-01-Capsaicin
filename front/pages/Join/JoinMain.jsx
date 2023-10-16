786555f5382e2c59ae9fbca5b4c7870cfa085078
hooooooni <kheee2010@dgu.ac.kr>
2023-10-16 17:14:28 +0900

    Feat: 폰트 추가, Router 작업,  메인화면 서브 타이틀 제작

---
+++ front/pages/Join/JoinMain.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import * as S from "./style";
+
+export default function JoinPage() {
+    return (
+        <>
+            <S.SubTitle>내 손 안의 작은 스마트 경로당</S.SubTitle>
+        </>
+    )
+}
---
+++ front/pages/Join/style.jsx
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+export const SubTitle = styled.div`
+    border-radius: 10px;
+    text-align: center;
+    font-size: 20px;
+    color: #000000;
+`;
