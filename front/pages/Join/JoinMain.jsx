a2fbab0a97e4e2c10e445f882768b19e8e88b7ce
hooooooni <kheee2010@dgu.ac.kr>
2023-10-16 18:13:54 +0900

    Feat: 로그인 페이지그인 페이지 구현

--- front/pages/Join/JoinMain.jsx
+++ front/pages/Join/JoinMain.jsx
@@ -4,7 +4,25 @@ import * as S from "./style";
 export default function JoinPage() {
     return (
         <>
-            <S.SubTitle>내 손 안의 작은 스마트 경로당</S.SubTitle>
+            <S.Container>
+                <S.Title
+                    src="../../assets/images/login_box.png" />
+                <S.TitleSubText>내 손 안의 작은 스마트 경로당</S.TitleSubText>
+                <S.TitleSubTitle>고혈압, 스톱!</S.TitleSubTitle>
+
+                <S.Logo
+                    src="../../assets/images/logo_black.png" />
+
+                <S.Input>
+                    <S.InputID type="text" placeholder='아이디'></S.InputID>
+                    <S.InputPW type="password" placeholder='비밀번호'></S.InputPW>
+                </S.Input>
+
+                <S.LoginButton>로그인</S.LoginButton>
+
+                <S.Join>계정이 없으신가요? 회원가입</S.Join>
+                <S.IDfinder>아이디/비밀번호 찾기</S.IDfinder>
+            </S.Container>
         </>
-    )
+    );
 }
--- front/pages/Join/style.jsx
+++ front/pages/Join/style.jsx
@@ -1,8 +1,111 @@
 import styled from "styled-components";
 
-export const SubTitle = styled.div`
-    border-radius: 10px;
+export const Container = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    position: relative;
     text-align: center;
+`;
+
+
+export const Title = styled.img`
+    width: 390px;
+    height: 120px;
+    top: -40px;
+    left: 50%;
+    transform: translateX(-50%); /* 이미지를 수평으로 가운데 정렬 */
+    position: absolute;
+    z-index: 1;
+`;
+
+export const TitleSubText = styled.div`
+    font-family: 'NanumSquareRoundEB';
     font-size: 20px;
-    color: #000000;
+    text-align: center;
+    position: relative;
+    z-index: 2;
+    color: #FFFFFF;
+`;
+
+export const TitleSubTitle = styled.div`
+    font-family: 'NanumSquareRoundEB';
+    font-size: 30px;
+    text-align: center;
+    position: relative;
+    z-index: 2;
+    color: #FFFFFF;
+    padding: 5px 0px 0px 0px;
+`;
+
+export const Logo = styled.img`
+    width: 300px;
+    height: 85px;
+    padding: 80px 0px 50px 0px;
+`;
+
+
+export const Input = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+`;
+
+export const InputID = styled.input`
+    font-family: 'Pretendard';
+    width: 340px;
+    height: 85px;
+    border: 0.5px solid black;
+    border-radius: 5px;
+    font-size: 25px;
+    ::placeholder {
+        font-family: 'Pretendard';
+        font-size: 25px; 
+        color: #aaaaaa; 
+    }
+    padding-left: 10px;
+`;
+
+export const InputPW = styled.input`
+    font-family: 'Pretendard';
+    width: 340px;
+    height: 85px;
+    border: 0.5px solid black;
+    border-radius: 5px;
+    font-size: 25px;
+    ::placeholder {
+        font-family: 'Pretendard';
+        font-size: 25px; 
+        color: #aaaaaa; 
+    }
+    padding-left: 10px;
+    margin-top: 30px;
+`;
+
+export const LoginButton = styled.button`
+    font-family: 'Pretendard-Bold';
+    width: 350px;
+    height: 65px;
+    border-radius: 5px;
+    border-style: none;
+    font-size: 25px;
+    color: white;
+    background-color: #712EFF;
+    margin-top: 30px;
+`;
+
+export const Join = styled.div`
+    font-family: 'NotoSansKR-Bold';
+    font-size: 25px;
+    padding-top: 50px;
+    text-decoration: underline;
+`;
+
+export const IDfinder = styled.div`
+    font-family: 'NotoSansKR-Medium';
+    font-size: 25px;
+    padding-top: 20px;
+    text-decoration: underline;
 `;
