107670d5b75e74f89ab25a20366d77019f9143de
hooooooni <kheee2010@dgu.ac.kr>
2023-10-31 15:42:16 +0900

    Feat: 메인페이지 구현

---
+++ front/Components/Title.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import * as S from "./style";
+
+export default function Title() {
+    return (
+        <>
+            <S.Container>
+                <S.Title src="../../assets/images/join_box.png" />
+                <S.TitleLogo src="../../assets/images/logo_white.png" />
+            </S.Container>
+        </>
+    )
+}
--- front/src/App.jsx
+++ front/src/App.jsx
@@ -4,6 +4,61 @@ import LoginMain from '../pages/Join/LoginMain'
 import Join from '../pages/Join/Join'
 import './App.css'
 import NormalJoin from '../pages/Join/Term'
+import Main from '../pages/Main/Main'
+import { createGlobalStyle } from "styled-components";
+import styled from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+  html, body, div, span, applet, object, iframe,
+  h1, h2, h3, h4, h5, h6, p, blockquote, pre,
+  a, abbr, acronym, address, big, cite, code,
+  del, dfn, em, img, ins, kbd, q, s, samp,
+  small, strike, strong, sub, sup, tt, var,
+  b, u, i, center,
+  dl, dt, dd, ol, ul, li,
+  fieldset, form, label, legend,
+  table, caption, tbody, tfoot, thead, tr, th, td,
+  article, aside, canvas, details, embed, 
+  figure, figcaption, footer, header, hgroup, 
+  menu, nav, output, ruby, section, summary,
+  time, mark, audio, video {
+    margin: 0;
+    padding: 0;
+    border: 0;
+    font-size: 100%;
+    font: inherit;
+    vertical-align: baseline;
+  }
+  /* HTML5 display-role reset for older browsers */
+  article, aside, details, figcaption, figure, 
+  footer, header, hgroup, menu, nav, section {
+    display: block;
+  }
+  body {
+    line-height: 1;
+  }
+  ol, ul {
+    list-style: none;
+  }
+  blockquote, q {
+    quotes: none;
+  }
+  blockquote:before, blockquote:after,
+  q:before, q:after {
+    content: '';
+    content: none;
+  }
+  table {
+    border-collapse: collapse;
+    border-spacing: 0;
+  }
+`;
+
+const Basic = styled.div`
+  /* background-color: #f1f0f0; */
+  width: 100vw;
+  height: 100vh;
+`;
 
 function App() {
 
@@ -15,9 +70,13 @@ function App() {
 
   return (
     <>
+      <GlobalStyle />
+      {/* <Basic> */}
       {/* <LoginMain></LoginMain> */}
       {/* <Join></Join> */}
-      <NormalJoin></NormalJoin>
+      {/* <NormalJoin></NormalJoin> */}
+      <Main></Main>
+      {/* </Basic> */}
     </>
   )
 }
