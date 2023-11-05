98cbf2ab98f2ec3aa525f563137c930d166ba3cb
hooooooni <kheee2010@dgu.ac.kr>
2023-11-06 02:14:41 +0900

    Feat: 몸무게 입력 페이지 구현

--- front/pages/Input/HealthInput/WeightInput/WeightInput.jsx
+++ front/pages/Input/HealthInput/WeightInput/WeightInput.jsx
@@ -12,6 +12,25 @@ export default function WeightInput() {
             <S.Container>
                 <Title />
 
+                <S.Info>
+                    <S.Backward src="../../../assets/images/backward.png" onClick={() => navigate(`/inputmain`)} />
+                    <S.InputTitle>몸무게 입력</S.InputTitle>
+                </S.Info>
+
+                <S.JoinBox>
+                    <S.JoinContent>측정일자</S.JoinContent>
+                    <S.JoinInput type="date"></S.JoinInput>
+                </S.JoinBox>
+
+
+                <S.JoinBox>
+                    <S.JoinContent>몸무게</S.JoinContent>
+                    <S.JoinInput type="text" placeholder='몸무게(kg 생략, 숫자만 작성)'></S.JoinInput>
+                </S.JoinBox>
+
+
+                <S.CompleteButton onClick={() => navigate(`/inputmain`)}>작성완료</S.CompleteButton>
+
 
                 <Nav />
             </S.Container>
--- front/pages/Input/HealthInput/WeightInput/style.jsx
+++ front/pages/Input/HealthInput/WeightInput/style.jsx
@@ -6,3 +6,63 @@ export const Container = styled.div`
     align-items: center;
     position: relative;
 `;
+
+export const Info = styled.div`
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    padding-top: 50px;
+    padding-right: 60px;
+`;
+
+export const Backward = styled.img`
+    width: 18px;
+    height: 26px;
+`;
+
+export const InputTitle = styled.div`
+    font-family: 'NotoSansKR-Bold';
+    font-size: 35px;
+    text-align: center;
+    padding-left: 60px;
+`;
+
+
+export const JoinBox = styled.div`
+    display: flex;
+    flex-direction: column;
+`;
+
+export const JoinContent = styled.div`
+    font-family: 'NotoSansKR-Bold';
+    font-size: 30px;
+    padding-top: 50px;
+    text-align: left;
+`;
+
+
+export const JoinInput = styled.input`
+    font-family: 'Pretendard';
+    width: 340px;
+    height: 85px;
+    border: 0.5px solid black;
+    border-radius: 5px;
+    font-size: 25px;
+    ::placeholder {
+        font-family: 'Pretendard';
+        font-size: 25px; 
+        color: #aaaaaa; 
+    }
+    padding-left: 10px;
+    margin-top: 20px;
+`;
+
+export const CompleteButton = styled.button`
+    width: 143px;
+    height: 65px;
+    font-size: 20px;
+    color: white;
+    background-color: #48268F;
+    border-radius: 5px;
+    margin-top: 50px;
+`;
