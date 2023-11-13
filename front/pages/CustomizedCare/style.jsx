62525ee7924dfa8b73efe8d3f5c7fe254797a414
hooooooni <kheee2010@dgu.ac.kr>
2023-11-14 01:44:21 +0900

    Feat: 맞춤춤케어 페이지 구현

---
+++ front/pages/CustomizedCare/style.jsx
@@ -0,0 +1,103 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    position: relative;
+`;
+
+export const InputTitle = styled.div`
+    font-family: 'NotoSansKR-Bold';
+    font-size: 35px;
+    padding-left: 30px;
+    justify-content: flex-start;
+    padding: 20px 0px 20px 0px;
+`;
+
+export const Recommend = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    justify-content: center;
+    width: 390px;
+    padding-left: 50px;
+`;
+
+export const RecommendTitle = styled.div`
+    font-family: 'NanumSquareRound';
+    font-weight: bold;
+    font-size: 25px;
+    padding-left: 30px;
+    padding: 40px 0px 20px 0px;
+`;
+
+export const RecommendFood = styled.div`
+    display: flex;
+    flex-direction: column;
+    width: 350px;
+    height: 250px;
+    border-radius: 5px;
+    box-shadow: 0px 2px 4px 0.25px #7c7c7c;
+    font-size: 35px;
+    font-family: 'Pretendard-Medium';
+    padding-left: 20px;
+    justify-content: center;
+    align-items: flex-start;
+`;
+
+
+
+export const RecommendFoodTitle = styled.div`
+    font-family: 'NotoSansKR-Bold';
+    font-size: 25px;
+    padding-bottom: 15px;
+`;
+
+export const RecommendFoodContent = styled.div`
+    font-family: 'NotoSansKR-Medium';
+    font-size: 18px;
+    padding-bottom: 30px;
+`;
+
+export const RecommendFoodName = styled.div`
+    font-family: 'NotoSansKR-Medium';
+    font-size: 20px;
+    text-align: left;
+    padding-bottom: 8px;
+`;
+
+// 운동
+export const RecommendExercise = styled.div`
+    display: flex;
+    flex-direction: column;
+    width: 350px;
+    height: 250px;
+    border-radius: 5px;
+    box-shadow: 0px 2px 4px 0.25px #7c7c7c;
+    font-size: 35px;
+    font-family: 'Pretendard-Medium';
+    padding-left: 20px;
+    justify-content: center;
+    align-items: flex-start;
+`;
+
+export const RecommendExerciseTitle = styled.div`
+    font-family: 'NotoSansKR-Bold';
+    font-size: 25px;
+    padding-bottom: 15px;
+`;
+
+export const RecommendExercisecontent = styled.div`
+    font-family: 'NotoSansKR-Medium';
+    font-size: 18px;
+    text-align: left;
+    padding-bottom: 30px;
+`;
+
+export const RecommendExerciseName = styled.div`
+    font-family: 'NotoSansKR-Medium';
+    font-size: 20px;
+    text-align: left;
+    padding-bottom: 8px;
+`;
