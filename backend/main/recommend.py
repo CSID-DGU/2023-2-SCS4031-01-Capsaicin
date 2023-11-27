c2b5bdfacb501e619206863e3e0da2967bc76ec3
Shinseungw0n <swon9178@naver.com>
2023-11-27 22:11:57 +0900

    csv파일 추가, 경로 수정

--- backend/main/recommend.py
+++ backend/main/recommend.py
@@ -1,9 +1,14 @@
 import pandas as pd
 import tabulate
+import os
+import pathlib
 
-df = pd.read_csv('C:\\Users\\Shin\\Downloads\\음식분류.csv',encoding='UTF-8') # csv 파일 읽어오기
 
-data = df[['분류', '음 식 명', '나트륨(mg)']] # 필요한 데이터만 가져오기
+PATH = pathlib.Path(__file__).parent.parent
+
+df = pd.read_csv(os.path.join(PATH, '음식분류.csv'),encoding='UTF-8') # csv 파일 읽어오기
+
+data = df[['분류', '음 식 명', '나트륨']] # 필요한 데이터만 가져오기
 
 from sklearn.feature_extraction.text import CountVectorizer
 
@@ -24,7 +29,7 @@ def recommend_menu(df, menu_name, top=10):
 	sim_idx = sim_idx[sim_idx != target_menu_idx]
 
 	result = df.iloc[sim_idx]
-	result = result.sort_values(by='나트륨(mg)', ascending=True)
+	result = result.sort_values(by='나트륨', ascending=True)
 
 	return result
 
@@ -52,7 +57,7 @@ from tabulate import tabulate
 recommended = recommend_menu(data, '치킨가스', top=10)
 
 # '나트륨'을 기준으로 데이터프레임을 정렬합니다.
-sorted_recommended = recommended[['음 식 명', '나트륨(mg)']].sort_values(by='나트륨(mg)')
+sorted_recommended = recommended[['음 식 명', '나트륨']].sort_values(by='나트륨')
 
 # tabulate를 사용하여 표 형식으로 출력합니다.
 table = tabulate(sorted_recommended, headers='keys', tablefmt='pretty')
