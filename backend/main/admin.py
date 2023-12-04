e987c7c0914ea51b875675309e1aef201099dbb6
kkong1007 <vkslvkd1007@naver.com>
2023-12-04 22:53:03 +0900

    Modify: 음식 카테고리 엑셀로 저장

--- backend/main/admin.py
+++ backend/main/admin.py
@@ -7,7 +7,7 @@ from import_export.admin import ImportExportModelAdmin
 # Register your models here.
 admin.site.register(BloodPressure)
 admin.site.register(Weight)
-admin.site.register(FoodCategory)
+# admin.site.register(FoodCategory)
 admin.site.register(Center)
 admin.site.register(Notice)
 # admin.site.register(ExerciseCategory)
@@ -24,6 +24,17 @@ class FoodAdmin(ImportExportModelAdmin):
     list_display = ('id','foodName', 'calorie', 'natrium', 'category', 'amount', 'foodImgUrl')
     resource_class = FoodResource
 
+class FoodCategoryResource(resources.ModelResource):
+    class Meta:
+        model = FoodCategory
+        fields = ('name','imgUrl')
+        export_order = fields
+
+class FoodCategoryAdmin(ImportExportModelAdmin):
+    fields = ('name','imgUrl')
+    list_display = ('id','name','imgUrl')
+    resource_class = FoodCategoryResource
+
 class ExerciseResource(resources.ModelResource):
     class Meta:
         model = ExerciseCategory
@@ -38,3 +49,4 @@ class ExerciseAdmin(ImportExportModelAdmin):
 
 admin.site.register(Food, FoodAdmin)
 admin.site.register(ExerciseCategory, ExerciseAdmin)
+admin.site.register(FoodCategory, FoodCategoryAdmin)
