863c37bbf5a17833e12230a919507c579ecee37a
hooooooni <kheee2010@dgu.ac.kr>
2023-12-06 15:49:27 +0900

    Feat: 로고 누르면 메인 페이지로 이동

--- front/Components/Title.jsx
+++ front/Components/Title.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import * as S from "./style";
+import { useNavigate } from "react-router-dom";
 
 export default function Title() {
+    const navigate = useNavigate();
     return (
         <>
             <S.Container>
                 <S.Title src="../../assets/images/join_box.png" />
-                <S.TitleLogo src="../../assets/images/logo_white.png" />
+                <S.TitleLogo src="../../assets/images/logo_white.png"
+                    onClick={() => navigate(`/main`)} />
             </S.Container>
         </>
     )
